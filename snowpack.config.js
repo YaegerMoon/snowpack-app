75ad4eee0592be2f447ec83678d8c64a1bf8331e
UigeoMun <yaeger@kakao.com>
2021-07-13 23:26:41 +0900

    React Base Setting

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,9 +5,12 @@
 module.exports = {
   mount: {
     /* ... */
+    public: "/",
+    src: "/dist"
   },
   plugins: [
     /* ... */
+    "@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv"
   ],
   packageOptions: {
     /* ... */
